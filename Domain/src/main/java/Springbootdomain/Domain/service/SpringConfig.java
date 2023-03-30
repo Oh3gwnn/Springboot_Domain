2288275e6012c70407eb74cd230f550fd59364bc
hyoungteak <aqko0719@gmail.com>
2023-03-30 16:43:35 +0900

    SpringData Jpa

--- Domain/src/main/java/Springbootdomain/Domain/service/SpringConfig.java
+++ Domain/src/main/java/Springbootdomain/Domain/service/SpringConfig.java
@@ -1,10 +1,6 @@
 package Springbootdomain.Domain.service;
 
-import Springbootdomain.Domain.repository.JdbcMemberRepository;
-import Springbootdomain.Domain.repository.JdbcTemplateMemberRepository;
-import Springbootdomain.Domain.repository.JpaMemberRepository;
 import Springbootdomain.Domain.repository.MemberRepository;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,22 +9,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+/**
     @Bean
     public MemberRepository memberRepository() {
         return new JpaMemberRepository(em);
-//      return new JdbcTemplateMemberRepository(dataSource);
-//      return new JdbcMemberRepository(dataSource);
-//      return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
+        return new JdbcMemberRepository(dataSource);
+        return new MemoryMemberRepository();
     }
+ **/
 }
