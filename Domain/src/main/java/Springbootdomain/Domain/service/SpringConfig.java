337dfa6acb8685fd7b93a11fb20f260b726c0d39
hyoungteak <aqko0719@gmail.com>
2023-03-30 14:59:25 +0900

    JDBC 연습

--- Domain/src/main/java/Springbootdomain/Domain/service/SpringConfig.java
+++ Domain/src/main/java/Springbootdomain/Domain/service/SpringConfig.java
@@ -1,14 +1,21 @@
 package Springbootdomain.Domain.service;
 
-import Springbootdomain.Domain.controller.MemberController;
+import Springbootdomain.Domain.repository.JdbcMemberRepository;
 import Springbootdomain.Domain.repository.MemberRepository;
-import Springbootdomain.Domain.repository.MemoryMemberRepository;
-import Springbootdomain.Domain.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+//      return new MemoryMemberRepository();
     }
 }
--- Domain/src/test/java/Springbootdomain/Domain/service/MemberServiceTest.java
+++ Domain/src/test/java/Springbootdomain/Domain/service/MemberServiceTest.java
@@ -5,9 +5,12 @@ import Springbootdomain.Domain.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 
 class MemberServiceTest {
 
